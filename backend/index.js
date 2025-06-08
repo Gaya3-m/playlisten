069a8881708ddea1b85fc7a8593d1e4c39ab81ef
Gaya3-m <gayathri2609z@gmail.com>
2025-06-08 16:45:58 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,14 @@ const httpServer=createServer(app);
 initializeSocket(httpServer);
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: function(origin, callback){
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials:true,
 }));
 
